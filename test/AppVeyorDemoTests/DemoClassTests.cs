b7e763dedc2246f5a7ef848abedc7fe80845018d
Jeremy Meng <yumeng@microsoft.com>
2016-07-22 10:55:38 -0700

    Add AppVeyorDemoTests project

---
+++ test/AppVeyorDemoTests/DemoClassTests.cs
@@ -0,0 +1,15 @@
+﻿using AppVeyorDemo;
+using Xunit;
+
+namespace AppVeyorDemoTests
+{
+    public class DemoClassTests
+    {
+        [Fact]
+        public void MeNeverFails_should_return_true()
+        {
+            var c = new DemoClass();
+            Assert.True(c.MeNeverFails(), "Expected true");
+        }
+    }
+}
